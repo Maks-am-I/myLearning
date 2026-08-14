99903c2c12b95ab5e421048d9506e4897b1e8126
Maks-am-I <newstepms@yandex.ru>
2026-08-14 07:14:18 +0700

    feat: continue Functions

--- freeCodeCamp/functions/2_labs.js
+++ freeCodeCamp/functions/2_labs.js
@@ -21,3 +21,56 @@ function maskEmail(email) {
 
 let email = 'mikc3000@mail.ru';
 console.log(maskEmail(email));
+
+// build a Celsius to Fahrenheit Converter
+function convertCtoF(cel) {
+  return cel * (9 / 5) + 32;
+}
+
+// build a Card Counting Assistant
+let count = 0;
+function cardCounter(card) {
+  let increasedList = '23456';
+  let decreasedList = '10JQKA';
+
+  if (increasedList.includes(card)) {
+    count++;
+  } else if (decreasedList.includes(card)) {
+    count--;
+  }
+
+  return count > 0 ? `${count} Bet` : `${count} Hold`;
+}
+
+// build a Leap Year Calculator
+function isLeapYear(number) {
+  let devisionByFour = number % 4 === 0;
+  let devisionByHundred = number % 100 === 0;
+  let devisionByFourhundred = number % 400 === 0;
+  console.log(devisionByFour, !devisionByHundred, devisionByFourhundred);
+
+  if (devisionByFour && !devisionByHundred || devisionByFourhundred) {
+    return `${number} is a leap year.`
+  } else {
+    return `${number} is not a leap year.`
+  } 
+}
+
+let year = 2000;
+let result = isLeapYear(year);
+console.log(result);
+
+// implement the Truncate String Algorithm
+function truncateString(str, num) {
+  let lengthStr = str.length;
+  let points = '...';
+  let newStr;
+
+  if (lengthStr > num) {
+    newStr = str.slice(0, num) + points;
+  } else {
+    newStr = str;
+  }
+
+  return newStr;
+}
